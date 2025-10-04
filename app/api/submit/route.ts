5b3bfc2510494dab334bf9c750a79e4966638687
IlliaLev <illialevuoi@gmail.com>
2025-10-04 18:05:19 +0300

    Initial commit

---
+++ app/api/submit/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const data = await request.json();
+    console.log('Received data: ', data);
+
+    return NextResponse.json({ message: "Data Received!", data: data});
+}
